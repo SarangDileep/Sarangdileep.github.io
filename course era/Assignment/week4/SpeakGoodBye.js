f6442765870367eb9aa57afc58a20b17465a982a
Sarang dileep <sarangdileep2771@gmail.com>
2020-05-07 10:21:11 +0530

    Create  SpeakGoodBye.js

---
+++ course era/Assignment/week4/SpeakGoodBye.js
@@ -0,0 +1,11 @@
+(function(window){
+  var speakWord = "Good Bye";
+  var byeSpeaker = {
+    speak : function(name) {
+      console.log(speakWord + " " + name);
+    }
+  };
+
+  window.byeSpeaker = byeSpeaker;
+}
+)(window);
