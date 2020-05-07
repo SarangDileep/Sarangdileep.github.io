762cb60d178af8357085fdf98c5e556e48d9c073
Sarang dileep <sarangdileep2771@gmail.com>
2020-05-07 10:25:54 +0530

    Create SpeakHello.js

---
+++ course era/Assignment/week4/SpeakHello.js
@@ -0,0 +1,8 @@
+(function (window) {
+    var speakWord = "Hello";
+    var helloSpeaker = {};
+    helloSpeaker.speak = function (name) {
+      console.log(speakWord + " " + name);
+    };
+    window.helloSpeaker = helloSpeaker;
+})(window);
