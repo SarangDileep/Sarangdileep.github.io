dadeb55d8324b1587a8efc82a167b1e2013a9835
SarangDileep <sarangdileep2771@gmail.com>
2021-07-26 11:07:30 +0530

    New webpage

--- course era/Assignment/week4/SpeakHello.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-    var speakWord = "Hello";
-    var helloSpeaker = {};
-    helloSpeaker.speak = function (name) {
-      console.log(speakWord + " " + name);
-    };
-    window.helloSpeaker = helloSpeaker;
-})(window);
